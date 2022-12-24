72bf592b586f87f1b064ffbd1ee24a8be2a423dc
Joshua Alexander <alexander.joshua2018@gmail.com>
2022-12-24 23:33:32 +0100

    configured environment variables

--- src/auth/passport/jwt.strategy.ts
+++ src/auth/passport/jwt.strategy.ts
@@ -1,13 +1,11 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
-import { jwtConstants } from '../constants/constants';
-import path from 'path';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly configService: ConfigService) {
+  constructor(private configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
